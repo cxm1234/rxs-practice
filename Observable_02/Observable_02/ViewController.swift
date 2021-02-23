c88f2f962e608585cccc917a7209152ff623c5c7
245413040 <245413040@qq.com>
2021-02-24 00:22:47 +0800

    pracice for observabel

--- Observable_02/Observable_02/ViewController.swift
+++ Observable_02/Observable_02/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        test2()
+    }
+}
 
+extension ViewController {
+    private func test1() {
+        print("start for test1")
+        let one = 1
+        let two = 2
+        let three = 3
+        let observable = Observable.just(one)
+        let observable2 = Observable.of(one, two, three)
+        let observable3 = Observable.of([one, two, three])
+        let observable4 = Observable.from([one, two, three])
+    }
+    
+    private func test2() {
+        print("start for test2")
+        let one = 1
+        let two = 2
+        let three = 3
+        
+        let observable = Observable.of(one, two, three)
+        _ = observable.subscribe { (event) in
+            print(event)
+        }
+    }
 }
 
