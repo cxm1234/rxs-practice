f2408151a3faa9b4f20b4f61cdd9a99643117489
245413040 <245413040@qq.com>
2021-02-23 23:57:59 +0800

    调整工程目录

---
+++ Observable_02/Observable_02Tests/Observable_02Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Observable_02Tests.swift
+//  Observable_02Tests
+//
+//  Created by xiaoming on 2021/2/23.
+//
+
+import XCTest
+@testable import Observable_02
+
+class Observable_02Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
