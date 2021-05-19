8c69da6d95275ffbd7601e5950072422bd43fe08
245413040 <245413040@qq.com>
2021-05-20 01:03:54 +0800

    behavious subject

--- Subjects_03/Subjects_03/ViewController.swift
+++ Subjects_03/Subjects_03/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 import RxSwift
 
+enum MyError: Error {
+    case anError
+}
+
+func print<T: CustomStringConvertible>(label: String, event: Event<T>) {
+    print(label, (event.element ?? event.error) ?? event)
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -16,7 +24,7 @@ class ViewController: UIViewController {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        test1()
+        test2()
     }
 
 }
@@ -25,13 +33,62 @@ extension ViewController {
     private func test1() {
         let subject = PublishSubject<String>()
         subject.onNext("Is anyone listening?")
-        let _ = subject
+        let subscriptionOne = subject
             .subscribe { (string) in
                 print(string)
             }
         
         subject.on(.next("1"))
         subject.onNext("2")
+        
+        
+        let subscriptionTwo = subject
+            .subscribe { event in
+                print("2)", event.element ?? event)
+            }
+        
+        subject.on(.next("3"))
+        
+        subscriptionOne.dispose()
+        
+        subject.onNext("4")
+        
+        subject.onCompleted()
+        
+        subject.onNext("5")
+        
+        subscriptionTwo.dispose()
+        
+        let disposeBag = DisposeBag()
+        
+        subject
+            .subscribe {
+                print("3)", $0.element ?? $0)
+            }
+            .disposed(by: disposeBag)
+        
+        subject.onNext("?")
+        
+    }
+    
+    private func test2() {
+        let subject = BehaviorSubject(value: "Initial value")
+        let disposeBag = DisposeBag()
+        
+        subject.subscribe {
+            print(label: "1)", event: $0)
+        }
+        .disposed(by: disposeBag)
+        
+        subject.onNext("X")
+        
+        subject.onError(MyError.anError)
+        
+        subject
+            .subscribe {
+                print(label: "2)", event: $0)
+            }
+            .disposed(by: disposeBag)
     }
 }
 
