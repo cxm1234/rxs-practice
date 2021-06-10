c69c952c338bbaabfbc56ff45e9fbca82e47a535
245413040 <245413040@qq.com>
2021-06-10 08:38:54 +0800

    start filter

---
+++ Filtering_05/Filtering_05/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Filtering_05
+//
+//  Created by xiaoming on 2021/6/9.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        testThree()
+    }
+
+}
+
+extension ViewController {
+    private func testOne() {
+        let strikes = PublishSubject<String>()
+        let disposeBag = DisposeBag()
+        strikes
+            .ignoreElements()
+            .subscribe {
+                print("You're out")
+            }
+            .disposed(by: disposeBag)
+        
+        strikes.onNext("X")
+        strikes.onNext("X")
+        strikes.onNext("X")
+        
+        strikes.onCompleted()
+
+    }
+    
+    private func testTwo() {
+        let strikes = PublishSubject<String>()
+        let disposeBag = DisposeBag()
+        
+        strikes
+            .elementAt(2)
+            .subscribe(onNext:{ _ in
+                print("You're out")
+            })
+            .disposed(by: disposeBag)
+        
+        strikes.onNext("X")
+        strikes.onNext("X")
+        strikes.onNext("X")
+
+    }
+    
+    private func testThree() {
+        let disposeBag = DisposeBag()
+        
+        Observable.of(1, 2, 3, 4, 5, 6)
+            .filter { $0.isMultiple(of: 2) }
+            .subscribe(onNext:{
+                print($0)
+            })
+            .disposed(by: disposeBag)
+            
+    }
+}
+
---
+++ Filtering_05/Pods/Target Support Files/Pods-Filtering_05-Filtering_05UITests/Pods-Filtering_05-Filtering_05UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Filtering_05_Filtering_05UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Filtering_05_Filtering_05UITests
+@end
