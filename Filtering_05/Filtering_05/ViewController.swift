097f8c38f07e6e1715d8dee403a04b57b951d02f
Generic <15395139180@163.com>
2021-06-11 11:54:37 +0800

    filer_05_finished

--- Filtering_05/Filtering_05/ViewController.swift
+++ Filtering_05/Filtering_05/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        testSix()
+        testTwelve()
     }
 
 }
@@ -113,6 +113,100 @@ extension ViewController {
         
         subject.onNext("C")
         
+    }
+    
+    private func testSeven() {
+        let disposeBag = DisposeBag()
+        Observable.of(1, 2, 3, 4, 5, 6)
+            .take(3)
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func testEight() {
+        let disposeBag = DisposeBag()
+        Observable.of(2, 2, 4, 4, 6, 6)
+            .enumerated()
+            .takeWhile { index, integer in
+                integer.isMultiple(of: 2) && index < 3
+            }
+            .map(\.element)
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func testNine() {
+        let disposeBag = DisposeBag()
+        Observable.of(1, 2, 3, 4, 5)
+            .takeUntil(.exclusive) { $0.isMultiple(of: 4) }
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func testTen() {
+        let disposeBag = DisposeBag()
+        let subject = PublishSubject<String>()
+        let trigger = PublishSubject<String>()
+        
+        subject
+            .takeUntil(trigger)
+            .subscribe(onNext:{
+                print($0)
+            })
+            .disposed(by: disposeBag)
+        
+        subject.onNext("1")
+        subject.onNext("2")
+        
+        trigger.onNext("X")
+        
+        subject.onNext("3")
+        
+    }
+
+    private func testEleven() {
+        let disposeBag = DisposeBag()
+        Observable.of("A", "A", "B", "B", "A")
+            .distinctUntilChanged()
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+        
+    }
+    
+    private func testTwelve() {
+        let disposeBag = DisposeBag()
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .spellOut
+        
+        Observable<NSNumber>.of(10, 110, 20, 200, 210, 310)
+            .distinctUntilChanged { a, b in
+                guard let aWords = formatter.string(from: a)?.components(separatedBy: " "),
+                      let bWords = formatter.string(from: b)?.components(separatedBy: " ") else {
+                    return false
+                }
+                
+                var containsMatch = false
+                
+                for aWord in aWords where bWords.contains(aWord) {
+                    containsMatch = true
+                    break
+                }
+                
+                return containsMatch
+            }
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
     }
 }
 
