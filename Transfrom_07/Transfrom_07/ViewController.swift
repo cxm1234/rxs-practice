273852d282e07eeb294cb664cc54c13472376c69
245413040 <245413040@qq.com>
2021-07-12 09:01:29 +0800

    start transform

--- Transfrom_07/Transfrom_07/ViewController.swift
+++ Transfrom_07/Transfrom_07/ViewController.swift
@@ -15,11 +15,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
-    touch
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        test2()
+    }
 }
 
 extension ViewController {
-    pri
+    
+    private func test1() {
+        let disposeBag = DisposeBag()
+        Observable.of("A", "B", "C")
+            .toArray()
+            .subscribe(onSuccess: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func test2() {
+        let disposeBag = DisposeBag()
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .spellOut
+        
+        Observable<Int>.of(123, 4, 56)
+            .map {
+                formatter.string(for: $0) ?? ""
+            }
+            .subscribe(onNext:{
+                print($0)
+            })
+            .disposed(by: disposeBag)
+    }
+    
 }
 
