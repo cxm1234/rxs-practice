59bc9128c1f316bf2a0aec44b25c5b55d57b2e20
chengxiaoming <chengxiaoming@top-stack.com>
2021-07-12 16:08:59 +0800

    07_finished

---
+++ Transfrom_07/Transfrom_07/Student.swift
@@ -0,0 +1,14 @@
+//
+//  Student.swift
+//  Transfrom_07
+//
+//  Created by  generic on 2021/7/12.
+//
+
+import Foundation
+import RxSwift
+
+struct Student {
+    let score: BehaviorSubject<Int>
+}
+
--- Transfrom_07/Transfrom_07/ViewController.swift
+++ Transfrom_07/Transfrom_07/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        test2()
+        test8()
     }
 }
 
@@ -46,6 +46,152 @@ extension ViewController {
             })
             .disposed(by: disposeBag)
     }
+    
+    private func test3() {
+        let disposeBag = DisposeBag()
+        
+        Observable.of(1, 2, 3, 4, 5, 6)
+            .enumerated()
+            .map { index, integer in
+                index > 2 ? integer * 2 : integer
+            }
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func test4() {
+        let disposeBag = DisposeBag()
+        Observable.of("To", "be", nil, "or", "not", "to", "be", nil)
+            .compactMap { $0 }
+            .toArray()
+            .map { $0.joined(separator: " ") }
+            .subscribe(onSuccess: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func test5() {
+        let disposeBag = DisposeBag()
+        let laura = Student(score: BehaviorSubject(value: 80))
+        let charlotte = Student(score: BehaviorSubject(value: 90))
+        let student = PublishSubject<Student>()
+        
+        student
+            .flatMap {
+                $0.score
+            }
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+        
+        student.onNext(laura)
+        laura.score.onNext(85)
+        student.onNext(charlotte)
+        laura.score.onNext(95)
+        charlotte.score.onNext(100)
+    }
+    
+    private func test6() {
+        let disposeBag = DisposeBag()
+        let laura = Student(score: BehaviorSubject(value: 80))
+        let charlotte = Student(score: BehaviorSubject(value: 90))
+        let student = PublishSubject<Student>()
+        
+        student
+            .flatMapLatest {
+                $0.score
+            }
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+        
+        student.onNext(laura)
+        laura.score.onNext(85)
+        student.onNext(charlotte)
+        
+        laura.score.onNext(95)
+        charlotte.score.onNext(100)
+    }
+    
+    private func test7() {
+        enum MyError: Error {
+            case anError
+        }
+        
+        let disposeBag = DisposeBag()
+        
+        let laura = Student(score: BehaviorSubject(value: 80))
+        let charlotte = Student(score: BehaviorSubject(value: 100))
+        
+        let student = BehaviorSubject(value: laura)
+        
+        let studentScore = student
+            .flatMapLatest {
+                $0.score
+            }
+            
+        studentScore
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+        
+        laura.score.onNext(85)
+        
+        laura.score.onError(MyError.anError)
+        
+        laura.score.onNext(90)
+        
+        student.onNext(charlotte)
+        
+    }
+    
+    private func test8() {
+        enum MyError: Error {
+            case anError
+        }
+        
+        let disposeBag = DisposeBag()
+        
+        let laura = Student(score: BehaviorSubject(value: 80))
+        let charlotte = Student(score: BehaviorSubject(value: 100))
+        
+        let student = BehaviorSubject(value: laura)
+        
+        let studentScore = student
+            .flatMapLatest {
+                $0.score.materialize()
+            }
+            
+        studentScore
+            .filter{
+                guard $0.error == nil else {
+                    print($0.error!)
+                    return false
+                }
+                return true
+            }
+            .dematerialize()
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+        
+        laura.score.onNext(85)
+        
+        laura.score.onError(MyError.anError)
+        
+        laura.score.onNext(90)
+        
+        student.onNext(charlotte)
+        
+    }
+    
     
 }
 
