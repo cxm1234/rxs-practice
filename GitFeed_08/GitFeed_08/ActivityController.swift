10aed7ed21ebe283770bf78b3d8d9590e8f94efd
245413040 <245413040@qq.com>
2021-07-17 17:33:54 +0800

    GitFeed Completed

--- GitFeed_08/GitFeed_08/ActivityController.swift
+++ GitFeed_08/GitFeed_08/ActivityController.swift
@@ -10,7 +10,19 @@ import RxSwift
 import RxCocoa
 import Kingfisher
 
+func cachedFileURL(_ fileName: String) -> URL {
+    return FileManager.default
+        .urls(for: .cachesDirectory, in: .allDomainsMask)
+        .first!
+        .appendingPathComponent(fileName)
+}
+
 class ActivityController: UITableViewController {
+    
+    private let modifiedFileURL = cachedFileURL("modified.txt")
+    
+    private let lastModified = BehaviorRelay<String?>(value: nil)
+    
     private let repo = "ReactiveX/RxSwift"
     
     private let events = BehaviorRelay<[Event]>(value: [])
@@ -28,13 +40,17 @@ class ActivityController: UITableViewController {
         refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
         refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
         
+        if let lastModifiedString = try? String(
+            contentsOf: modifiedFileURL, encoding: .utf8) {
+            lastModified.accept(lastModifiedString)
+        }
+        
         refresh()
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return events.value.count
     }
     
@@ -54,32 +70,80 @@ extension ActivityController {
     @objc func refresh() {
         DispatchQueue.global(qos: .default).async { [weak self] in
             guard let self = self else { return }
-            
+            self.fetchEvents(repo: self.repo)
         }
     }
     
     func fetchEvents(repo: String) {
-        let response = Observable.from(optional: [repo])
+        let response = Observable.from(["https://api.github.com/search/repositories?q=language:swift&per_page=5"])
             .map({ urlString -> URL in
-                return URL(string: "https://api.github.com/repos/\(urlString)/events")!
+                guard let url = URL(string: urlString) else {
+                    fatalError()
+                }
+                return url
             })
-            .map { url -> URLRequest in
-                return URLRequest(url: url)
+            .flatMap{ url -> Observable<Any> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.json(request: request)
             }
-            .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+            .flatMap { response -> Observable<String> in
+                guard let response = response as? [String: Any],
+                      let items = response["items"] as? [[String: Any]] else {
+                    return Observable.empty()
+                }
+                
+                return Observable.from(items.map {
+                    $0["full_name"] as! String
+                })
+            }
+            .map { urlString -> URL in
+                guard let url = URL(string: "https://api.github.com/repos/\(urlString)/events") else {
+                    fatalError()
+                }
+                return url
+            }
+            .map { [weak self] url -> URLRequest in
+                var request = URLRequest(url: url)
+                if let modifiedHeader = self?.lastModified.value {
+                    request.addValue(modifiedHeader, forHTTPHeaderField: "Last-Modified")
+                }
+                return request
+            }
+            .flatMap { request -> Observable<(response:HTTPURLResponse, data: Data)> in
                 return URLSession.shared.rx.response(request: request)
             }
             .share(replay: 1)
+            
+        
+        response
             .filter { response, _ in
-                return 200..<300 ~= response.statusCode
+            return 200..<300 ~= response.statusCode
+        }
+        .compactMap { _, data -> [Event]? in
+            return try? JSONDecoder().decode([Event].self, from: data)
+        }
+        .subscribe(onNext: { [weak self] newEvents in
+            self?.processEvents(newEvents)
+        })
+        .disposed(by: bag)
+        
+        response
+            .filter { response, _ in
+                return 200..<400 ~= response.statusCode
             }
-            .compactMap { _, data -> [Event]? in
-                return try? JSONDecoder().decode([Event].self, from: data)
+            .flatMap { response, _ -> Observable<String> in
+                guard let value = response.allHeaderFields["Last-Modified"] as? String else {
+                    return Observable.empty()
+                }
+                return Observable.just(value)
             }
-            .subscribe(onNext: { [weak self] newEvents in
-                self?.processEvents(newEvents)
+            .subscribe(onNext: { [weak self] modifiedHeader in
+                guard let self = self else { return }
+                self.lastModified.accept(modifiedHeader)
+                try? modifiedHeader.write(to: self.modifiedFileURL, atomically: true, encoding: .utf8)
             })
             .disposed(by: bag)
+        
     }
     
     func processEvents(_ newEvents: [Event]) {
@@ -88,6 +152,9 @@ extension ActivityController {
             updatedEvents = [Event](updatedEvents.prefix(upTo: 50))
         }
         events.accept(updatedEvents)
-        tableView.reloadData()
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.refreshControl?.endRefreshing()
+        }
     }
 }
