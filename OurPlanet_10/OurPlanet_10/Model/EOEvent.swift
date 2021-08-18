ae0e6b3e44cde196073160599ecbe23dfff1f926
245413040 <245413040@qq.com>
2021-08-18 21:22:32 +0800

    EOEvent

---
+++ OurPlanet_10/OurPlanet_10/Model/EOEvent.swift
@@ -0,0 +1,14 @@
+//
+//  EOEvent.swift
+//  OurPlanet_10
+//
+//  Created by xiaoming on 2021/8/18.
+//
+
+import Foundation
+
+struct EOEvent: Decodable {
+    let id: String
+    let title: String
+    let description: String
+}
