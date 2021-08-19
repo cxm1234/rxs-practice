bbdfdaa1fe055a2962dbfe33a456937f6a84e8e2
245413040 <245413040@qq.com>
2021-08-20 00:35:11 +0800

    start download

--- OurPlanet_10/OurPlanet_10/CategoriesViewController.swift
+++ OurPlanet_10/OurPlanet_10/CategoriesViewController.swift
@@ -7,30 +7,48 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class CategoriesViewController: UIViewController {
 
+    let categories = BehaviorRelay<[EOCategory]>(value: [])
+    let disposeBag = DisposeBag()
+    
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         startDownload()
     }
     
     func startDownload() {
-    
+        let eoCategories = EONET.categories
+        eoCategories
+            .bind(to: categories)
+            .disposed(by: disposeBag)
+        
+        categories
+            .asObservable()
+            .subscribe(onNext: { [weak self] _ in
+                DispatchQueue.main.async {
+                    self?.tableView?.reloadData()
+                }
+            })
+            .disposed(by: disposeBag)
     }
 
 }
 
 extension CategoriesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return categories.value.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell")!
+        let category = categories.value[indexPath.row]
+        cell.textLabel?.text = category.name
+        cell.detailTextLabel?.text = category.description
         return cell
     }
     
