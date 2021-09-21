ae18c3decefac7ed60017a57c85755acd7970838
245413040 <245413040@qq.com>
2021-09-21 23:42:31 +0800

    continue

--- iGif_17/iGif_17/Controller/ApiController.swift
+++ iGif_17/iGif_17/Controller/ApiController.swift
@@ -11,7 +11,7 @@ import RxSwift
 class ApiController {
     static let shared = ApiController()
     
-    private let apiKey = ""
+    private let apiKey = "RmBBAiXMMarfj17jubeJUJjNxo2R1xPA"
     
     func search(text: String) -> Observable<[GiphyGif]> {
         let url = URL(string: "http://api.giphy.com/v1/gifs/search")
