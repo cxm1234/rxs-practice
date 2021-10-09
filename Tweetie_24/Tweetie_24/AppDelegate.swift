4b9f79ec541a1435b5da0c5b000b9d21669f8dc5
245413040 <245413040@qq.com>
2021-10-09 09:04:43 +0800

    start tweeie

---
+++ Tweetie_24/Tweetie_24/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Tweetie_24
+//
+//  Created by xiaoming on 2021/10/9.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
